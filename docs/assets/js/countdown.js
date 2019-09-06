87bd27e1d6505238aafa4ed4fded32aabb811ccc
Ilkka Rinne <ilkka.rinne@spatineo.com>
2019-09-06 17:51:09 +0300

    Tried to make the countdown a bit smarter, still relies on the browser clock

--- docs/assets/js/countdown.js
+++ docs/assets/js/countdown.js
@@ -1,20 +1,48 @@
 $(function() {
+  var _endTime = (new Date("2019-10-06T16:00:00+02:00")).getTime();
+  var _lastSyncDiff = 0;
+  var _now = (new Date()).getTime();
+  syncCurrentTime();
+  _lastSyncDiff = 0;
+
+  function syncCurrentTime() {
+    /*
+    var nowWas = _now;
+    $.ajax({
+      url: "http://worldclockapi.com/api/jsonp/utc/now",
+      jsonp: "callback",
+      dataType: "jsonp",
+      success: function( response ) {
+        console.log("got response, now " + _now + "was:" + nowWas);
+        if (response.currentDateTime) {
+          var newTime = (new Date(response.currentDateTime)).getTime();
+          _lastSyncDiff = newTime - nowWas;
+          _now = newTime;
+          console.log("Synced, diff:" + _lastSyncDiff + ", now:" + _now);
+        }
+      }
+    });
+    */
+    var newTime = (new Date()).getTime();
+    _lastSyncDiff = newTime - _now;
+    _now = newTime;
+  }
+
   function formatUnit(base, value) {
     var retval = "<span>" + base;
-    if (value > 1) {
+    if (value !== 1) {
       retval += "s";
     }
     return retval + "</span>";
   }
 
-  function makeTimer() {
-        var endTime = new Date("2019-10-06T16:00:00+02:00");
-        endTime = (Date.parse(endTime) / 1000);
+  function addSecond() {
+    _now = _now + (1000 - _lastSyncDiff);
+    _lastSyncDiff = 0;
+  }
 
-        var now = new Date();
-        now = (Date.parse(now) / 1000);
-
-        var timeLeft = Math.max(endTime - now, 0);
+  function updateUI() {
+        var timeLeft = Math.max((_endTime - _now)/1000.0, 0);
         var days = Math.floor(timeLeft / 86400);
         var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
         var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
@@ -25,8 +53,10 @@ $(function() {
         $(".minutes").html(minutes + formatUnit("min", minutes));
         $(".seconds").html(seconds + formatUnit("sec", seconds));
     }
+
     if ($("#countdown").length) {
-      makeTimer();
-      setInterval(function() { makeTimer(); }, 1000);
+      updateUI();
+      setInterval(function() { addSecond(); updateUI(); }, 1000);
+      setInterval(function() { syncCurrentTime(); }, 10000);
     }
 });
