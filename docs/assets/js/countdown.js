0d30e75d6b4e173961f266ceac9865cb1044f7cc
Ilkka Rinne <ilkka.rinne@spatineo.com>
2019-09-07 04:06:27 -0600

    Server time sync now works

--- docs/assets/js/countdown.js
+++ docs/assets/js/countdown.js
@@ -1,31 +1,39 @@
 $(function() {
-  var _endTime = (new Date("2019-10-06T16:00:00+02:00")).getTime();
-  var _lastSyncDiff = 0;
-  var _now = (new Date()).getTime();
-  syncCurrentTime();
-  _lastSyncDiff = 0;
+  var _state = {
+    endTime: (new Date("2019-10-06T16:00:00+02:00")).getTime(),
+    pendingOffset: 0,
+    lastTick: Date.now(),
+    now: Date.now()
+  };
 
-  function syncCurrentTime() {
-    /*
-    var nowWas = _now;
+  function updateOffset(offset, requestTime) {
+    _state.pendingOffset = offset - requestTime / 2.0;
+  }
+
+  syncCurrentTime(_state.now, updateOffset);
+
+  function syncCurrentTime(myTime, callback) {
+    var start = Date.now();
     $.ajax({
-      url: "http://worldclockapi.com/api/jsonp/utc/now",
+      url: "https://www.spatineo.com/time/",
       jsonp: "callback",
       dataType: "jsonp",
-      success: function( response ) {
-        console.log("got response, now " + _now + "was:" + nowWas);
-        if (response.currentDateTime) {
-          var newTime = (new Date(response.currentDateTime)).getTime();
-          _lastSyncDiff = newTime - nowWas;
-          _now = newTime;
-          console.log("Synced, diff:" + _lastSyncDiff + ", now:" + _now);
+      data: { mine: myTime },
+      success: function( offset ) {
+        var requestTime = Date.now() - start;
+        var numOffset = parseFloat(offset);
+        if (!Number.isNaN(numOffset)) {
+          callback(numOffset, requestTime);
+        } else {
+          console.log("time sync did not return a float");
+          callback(0,0);
         }
+      },
+      error: function() {
+        console.log("time sync did not succeed");
+        callback(0,0);
       }
     });
-    */
-    var newTime = (new Date()).getTime();
-    _lastSyncDiff = newTime - _now;
-    _now = newTime;
   }
 
   function formatUnit(base, value) {
@@ -36,27 +44,29 @@ $(function() {
     return retval + "</span>";
   }
 
-  function addSecond() {
-    _now = _now + (1000 - _lastSyncDiff);
-    _lastSyncDiff = 0;
-  }
-
   function updateUI() {
-        var timeLeft = Math.max((_endTime - _now)/1000.0, 0);
-        var days = Math.floor(timeLeft / 86400);
-        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+    var timeLeft = Math.max((_state.endTime - _state.now)/1000.0, 0);
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-        $(".days").html(days + formatUnit("day", days));
-        $(".hours").html(hours + formatUnit("hr", hours));
-        $(".minutes").html(minutes + formatUnit("min", minutes));
-        $(".seconds").html(seconds + formatUnit("sec", seconds));
-    }
-
-    if ($("#countdown").length) {
+    $(".days").html(days + formatUnit("day", days));
+    $(".hours").html(hours + formatUnit("hr", hours));
+    $(".minutes").html(minutes + formatUnit("min", minutes));
+    $(".seconds").html(seconds + formatUnit("sec", seconds));
+  }
+  if ($("#countdown").length) {
+    updateUI();
+    setInterval(function() {
+      var now = Date.now();
+      _state.now = _state.now + (now - _state.lastTick) + _state.pendingOffset;
+      _state.lastTick = now;
+      _state.pendingOffset = 0;
       updateUI();
-      setInterval(function() { addSecond(); updateUI(); }, 1000);
-      setInterval(function() { syncCurrentTime(); }, 10000);
-    }
+    }, 1000);
+    setInterval(function() {
+      syncCurrentTime(_state.now, updateOffset);
+    }, 60000);
+  }
 });
